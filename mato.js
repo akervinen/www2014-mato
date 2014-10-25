c366fdc698d37d9ba88d6e22edc8b83882a7ddf0
Aleksi Kervinen <aleksi.kervinen@lut.fi>
2014-10-26 00:45:47 +0300

    added Mato drawing, fixed some function names, tweaked canvas height
    changed height for rounder cell size (16px)

--- mato.js
+++ mato.js
@@ -1,37 +1,75 @@
 function Mato(ctx) {
 	"use strict";
 
+	// Save this for inner functions
+	var self = this;
+
 	//-- Engine variables
-	// time stuff
+	// Time stuff
 	var currentTime = 0,
 		lastTime = (new Date()).getTime();
 
-	// canvas context
+	// Canvas context
 	this.context = ctx;
+
+	// Cell stuff
+	this.cellSize = 16; // pixels
+	this.width = 800/16; // cells
+	this.height = 640/16; // cells
+
+	// Get the x,y of cell's center
+	var getCellPos = function getCellPos(x, y) {
+		return {
+			x: x * self.cellSize + self.cellSize/2,
+			y: y * self.cellSize + self.cellSize/2
+		};
+	};
 
 	//-- Gameplay variables
 
-	// Snake's position, measured in cells
-	this.x = 0;
-	this.y = 0;
+	this.mato = {
+		direction: 'right',
+		// Mato's position, measured in cells
+		head: { x: 8, y: 8 },
+		tail: { x: 4, y: 8 },
+		// All points where Mato has turned
+		turns: []
+	};
 
-	// how much the snake has eaten (and how long it is)
-	this.eaten = 0;
+	// How much Mato has eaten (and how long it is)
+	this.eaten = 4;
 
-	// save this for loops
-	var self = this;
-
-	// updating and drawing
-	var update = function draw(delta) {
+	// Updating and drawing
+	var update = function update(delta) {
 		self.texthing = 'Frametime: ' + delta;
 	};
 
-	var draw = function update(delta) {
+	var draw = function draw(delta) {
 		ctx.save();
 		ctx.clearRect(0, 0, 800, 160);
 
 		ctx.font = '12pt sans-serif';
 		ctx.fillText(self.texthing, 10, 24);
+
+		// draw Mato
+		var m = self.mato;
+
+		ctx.fillStyle = 'black';
+		ctx.strokeStyle = 'black';
+		ctx.lineWidth = 16;
+
+		var head = getCellPos(m.head.x, m.head.y);
+		var tail = getCellPos(m.tail.x, m.tail.y);
+		var turn;
+
+		ctx.beginPath();
+		ctx.moveTo(head.x, head.y);
+		m.turns.forEach(function(t) {
+			turn = getCellPos(t.x, t.y);
+			ctx.lineTo(turn.x, turn.y);
+		});
+		ctx.lineTo(tail.x, tail.y);
+		ctx.stroke();
 
 		ctx.restore();
 	};
@@ -40,7 +78,7 @@ function Mato(ctx) {
 		// TODO: Polyfill this?
 		window.requestAnimationFrame(loop);
 
-		// update time stuff
+		// Update time stuff
 		currentTime = (new Date()).getTime();
 		var delta = (currentTime - lastTime)/1000;
 
@@ -50,7 +88,7 @@ function Mato(ctx) {
 		lastTime = currentTime;
 	};
 
-	// Feed the snake
+	// Feed Mato
 	this.eat = function eat() {
 		this.eaten++;
 	};

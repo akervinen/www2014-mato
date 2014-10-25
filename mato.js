4f32527bce7a89c2d75d796c3658ac5c7b4cbc31
Aleksi Kervinen <aleksi.kervinen@lut.fi>
2014-10-26 01:34:06 +0300

    implemented simple movement

--- mato.js
+++ mato.js
@@ -26,8 +26,8 @@ function Mato(ctx) {
 	};
 
 	var directions = {
-		up: { x: 0, y: 1 },
-		down: { x: 0, y: -1 },
+		up: { x: 0, y: -1 },
+		down: { x: 0, y: 1 },
 		left: { x: -1, y: 0 },
 		right: { x: 1, y: 0 }
 	};
@@ -51,30 +51,82 @@ function Mato(ctx) {
 
 	this.mato = {
 		direction: 'right',
+		lastDirection: 'right',
 		// Mato's position, measured in cells
 		head: { x: 8, y: 8 },
 		tail: { x: 4, y: 8 },
 		// All points where Mato has turned
 		turns: [],
+		// How many cells per second Mato moves
+		speed: 4,
 		// How much Mato has eaten (and how long it is)
-		eaten: 4
+		eaten: 4,
+
+		setDirection: function setDirection(dir) {
+			if (dir !== this.lastDirection && dir !== opposites[this.lastDirection]) {
+				this.direction = dir;
+			}
+		},
+		getNextHeadPos: function getNextHeadPos() {
+			return {
+				x: this.head.x + directions[this.direction].x,
+				y: this.head.y + directions[this.direction].y
+			};
+		},
+		getNextTailPos: function getNextTailPos() {
+			return {
+				x: this.tail.x + directions[this.direction].x,
+				y: this.tail.y + directions[this.direction].y
+			};
+		},
+		move: function move() {
+			this.head = this.getNextHeadPos();
+			this.tail = this.getNextTailPos();
+			this.lastDirection = this.direction;
+		}
 	};
 
-	// Updating and drawing
-	var update = function update(delta) {
+	//-- Gameplay functions
+
+	//-- Window events
+	window.onkeydown = function onkeydown(e) {
+		var dir = keys[e.keyCode];
+		if (dir) {
+			self.mato.setDirection(dir);
+			e.preventDefault();
+		}
+	};
+
+	//-- Updating and drawing
+	var lastMove = 0;
+	var update = function update(delta, curTime) {
 		// Update debug text
 		self.debugText = [
 			'Frametime: ' + delta,
 			'Length: ' + self.mato.eaten,
-			'Position: ' + self.mato.head.x + ', ' + self.mato.head.y
+			'Position: ' + self.mato.head.x + ', ' + self.mato.head.y,
+			'Direction: ' + self.mato.direction
 		];
 
-
+		// When entering new cell, change direction if needed
+		if ((curTime - lastMove) > (1000/self.mato.speed)) {
+			self.mato.move();
+			lastMove = curTime;
+		}
 	};
 
-	var draw = function draw(delta) {
+	var lerp = function lerp(start, end, amount) {
+		return {
+			x: start.x + (end.x - start.x) * amount,
+			y: start.y + (end.y - start.y) * amount
+		};
+	};
+
+	var lerpAmount = 0,
+		lastPos;
+	var draw = function draw(delta, curTime) {
 		ctx.save();
-		ctx.clearRect(0, 0, 800, 160);
+		ctx.clearRect(0, 0, 800, 640);
 
 		// Debug text
 		var textY = 24;
@@ -92,9 +144,28 @@ function Mato(ctx) {
 		ctx.strokeStyle = 'black';
 		ctx.lineWidth = 16;
 
-		var head = getCellPos(m.head.x, m.head.y);
-		var tail = getCellPos(m.tail.x, m.tail.y);
+		if (m.head !== lastPos) {
+			lerpAmount = 0;
+		} else {
+			// Animate movement using simple lerp
+			lerpAmount += delta * m.speed;
+			if (lerpAmount > 1) {
+				lerpAmount = 1;
+			}
+		}
+
+		var head = getCellPos(m.head.x, m.head.y),
+			tail = getCellPos(m.tail.x, m.tail.y);
+		var nextHead = m.getNextHeadPos(),
+			nextTail = m.getNextTailPos();
+		nextHead = getCellPos(nextHead.x, nextHead.y);
+		nextTail = getCellPos(nextTail.x, nextTail.y);
 		var turn;
+
+		lastPos = m.head;
+
+		//head = lerp(head, nextHead, lerpAmount);
+		//tail = lerp(tail, nextTail, lerpAmount);
 
 		ctx.beginPath();
 		ctx.moveTo(head.x, head.y);
@@ -116,8 +187,8 @@ function Mato(ctx) {
 		currentTime = (new Date()).getTime();
 		var delta = (currentTime - lastTime)/1000;
 
-		update(delta);
-		draw(delta);
+		update(delta, currentTime);
+		draw(delta, currentTime);
 
 		lastTime = currentTime;
 	};
